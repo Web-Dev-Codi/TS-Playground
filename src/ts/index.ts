ceab5579ccf232aa518c4dcc08983c513527ca28
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-11-17 16:37:06 +0100

    On too pizza site

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,8 @@
 
+import { Pizza } from "./models/Pizza";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    // Load Pizza Data
+    const pizzas = await Pizza.loadAll()
+    console.log("pizzas:", pizzas);
+})
---
+++ src/ts/services/DataResource.ts
@@ -0,0 +1,27 @@
+// export generic class to be used in our web app
+
+export class DataResource<T> {
+    constructor(private endpoint: string) { }
+    // Loads the PizzaProps endpoint and returns the array of pizzas
+    async loadAll(): Promise<T[]> {
+        const res = await fetch(this.endpoint);
+
+        return res.json();
+    }
+    async loadOne(id: number): Promise<T> {
+        const res = await fetch(`${this.endpoint}/${id}`);
+
+        return res.json();
+    }
+    async delete(id: number): Promise<Response> {
+        const res = await fetch(`${this.endpoint}/${id}`, { method: "DELETE" });
+
+        return res;
+    }
+    async save(data: T) {
+        const res = await fetch(this.endpoint, { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } });
+
+        return res;
+    }
+}
+
