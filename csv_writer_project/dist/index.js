cd47ea1a301ce4a0f3ebcb6bb6132b1ed9df4351
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-11-20 14:44:25 +0100

    Finished CSV Writer Module

--- csv_writer_project/dist/index.js
+++ csv_writer_project/dist/index.js
@@ -1,13 +1,25 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
 class CSVWriter {
     constructor(columns) {
         this.columns = columns;
-        this.csv = this.columns.join(',');
+        this.csv = this.columns.join(',') + '\n';
+    }
+    save(filename) {
+        (0, fs_1.appendFileSync)(filename, this.csv);
+        this.csv = '\n';
+        console.log("file saved to", filename);
     }
     addRows(values) {
+        let rows = values.map(v => this.formatRow(v));
+        this.csv += rows.join('\n');
+        console.log(this.csv);
     }
     formatRow(p) {
-        return this.columns.map(col => p[col]).join;
+        return this.columns.map(col => p[col]).join(',');
     }
 }
 const writer = new CSVWriter(['id', 'amount', 'to', 'notes']);
+writer.addRows([{ id: 1, amount: 100, to: "Gülden", notes: "Hugs and Kisses" }, { id: 2, amount: 200, to: "Brian", notes: "More Hugs and Kisses" }]);
+writer.save('./data/payments.csv');
--- csv_writer_project/src/index.ts
+++ csv_writer_project/src/index.ts
@@ -1,3 +1,5 @@
+import { appendFileSync } from 'fs';
+
 interface Payment {
 	id: number,
 	amount: number,
@@ -5,22 +7,35 @@ interface Payment {
 	notes: string,
 }
 
-type PaymentColumns = ('id'| 'amount'| 'to' | 'notes')[];
+type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[];
 
 class CSVWriter {
 	constructor(private columns: PaymentColumns) {
-		this.csv = this.columns.join(',')
-	 }
+		this.csv = this.columns.join(',') + '\n';
+	}
 
 	private csv: string;
 
-	addRows(values: Payment[]): void {
-
+	save(filename: string): void {
+		appendFileSync(filename, this.csv);
+		this.csv = '\n';
+		console.log("file saved to", filename);
 	}
 
-	private formatRow(p: Payment):string {
-		return this.columns.map(col => p[col]).join(',')
+	addRows(values: Payment[]): void {
+		let rows = values.map(v => this.formatRow(v));
+
+		this.csv += rows.join('\n');
+		console.log(this.csv);
+	}
+
+	private formatRow(p: Payment): string {
+		return this.columns.map(col => p[col]).join(',');
 	}
 }
 
-const writer = new CSVWriter(['id' , 'amount' , 'to' , 'notes'])
+const writer = new CSVWriter(['id', 'amount', 'to', 'notes']);
+
+writer.addRows([{ id: 1, amount: 100, to: "Gülden", notes: "Hugs and Kisses" }, { id: 2, amount: 200, to: "Brian", notes: "More Hugs and Kisses" }]);
+
+writer.save('./data/payments.csv');
